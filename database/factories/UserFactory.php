9b08683a0d1371e66918fb6ac1cf3cc01497109a
pazgalev <m.pazgalev@oyster-telecom.ru>
2019-12-17 12:27:12 +0300

    Update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->firstName,
         'surname' => $faker->lastName,
-        'login' => $faker->unique()->word,
+        'login' => $faker->userName,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
     ];
 });
