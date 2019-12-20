71684a83cd934247473d82c6b59cbe100087fbc9
pazgalev <m.pazgalev@oyster-telecom.ru>
2019-12-20 14:48:47 +0300

    Refactoring migrations

--- database/migrations/2019_12_19_172644_add_company_id_to_users_table.php
+++ database/migrations/2019_12_19_172644_add_company_id_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCompanyUsersTable extends Migration
+class AddCompanyIdToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,15 @@ class CreateCompanyUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('company_users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('company_id');
-            $table->unsignedBigInteger('user_id');
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('company_id')
+                ->after('last_ip')
+                ->nullable();
+
 
             $table->foreign('company_id')
                 ->references('id')
                 ->on('companies')
-                ->onDelete('cascade');
-
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
                 ->onDelete('cascade');
         });
     }
@@ -37,6 +33,9 @@ class CreateCompanyUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('company_users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_company_id_foreign');
+            $table->removeColumn('company_id');
+        });
     }
 }
