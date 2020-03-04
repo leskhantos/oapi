92e47ff35306d56b9ea394e0334ec0fc950e070a
roman <r.zubik@oyster-telecom.ru>
2020-03-04 17:56:20 +0300

    fxs date Guest

--- app/Entities/Guests/Guest.php
+++ app/Entities/Guests/Guest.php
@@ -8,6 +8,10 @@ class Guest extends Model
 {
     public $timestamps = false;
 
+    protected $casts = [
+        'datetime' => 'datetime:d-m-Y H:m:s',
+    ];
+
     protected $fillable =[
         'datetime','spot_id',
         'company_id','spot_type',
