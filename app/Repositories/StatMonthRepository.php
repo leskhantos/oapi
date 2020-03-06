388134cf69fc4ba1cdcfd352fecf9fce6ccf90ad
roman <r.zubik@oyster-telecom.ru>
2020-03-06 11:54:48 +0300

    :recycle: Refactoring stats

--- app/Repositories/StatMonthRepository.php
+++ app/Repositories/StatMonthRepository.php
@@ -19,21 +19,15 @@ class StatMonthRepository implements StatMonthRepositoryInterface
     {
         $new = new Helper();
         $myDate = $new->currentDate($request);
-        $call = StatsCall::select('date', 'requests', 'checked')->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
-        $voucher = StatsVoucher::select('date', 'all as all_vouchers', 'auth')->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
-        $sms = StatsSms::select('date', 'all as all_sms', 'resend', 'delivered')->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
+        $call = $this->getCall($myDate)->get()->toArray();
+        $voucher = $this->getVoucher($myDate)->get()->toArray();
+        $sms = $this->getSms($myDate)->get()->toArray();
 
         $vouchers = $this->counterMonth($voucher, $myDate['day']);
         $count_sms = $this->counterMonth($sms, $myDate['day']);
         $calls = $this->counterMonth($call, $myDate['day']);
 
-        return @response(['vouchers' => $vouchers, 'sms' => $count_sms, 'calls' => $calls, 'days' => $myDate['day']]);
+        return response(['vouchers' => $vouchers, 'sms' => $count_sms, 'calls' => $calls, 'days' => $myDate['day']]);
     }
 
 
@@ -42,24 +36,15 @@ class StatMonthRepository implements StatMonthRepositoryInterface
         $new = new Helper();
         $myDate = $new->currentDate($request);
         $company = Company::findOrFail($id);
-        $call = StatsCall::select('date', 'requests', 'checked')->whereCompany_id($company->id)
-            ->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
-        $guest = StatsGuest::select('date', 'load as load_guests', 'auth as auth_guests', 'new', 'old')->whereCompany_id($company->id)
-            ->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
-        $voucher = StatsVoucher::select('date', 'all', 'auth')->whereCompany_id($company->id)
-            ->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
+        $call = $this->getCall($myDate)->whereCompany_id($company->id)->get()->toArray();
+        $guest = $this->getGuest($myDate)->whereCompany_id($company->id)->get()->toArray();
+        $voucher = $this->getVoucher($myDate)->whereCompany_id($company->id)->get()->toArray();
 
         $vouchers = $this->counterMonth($voucher, $myDate['day']);
         $guests = $this->counterMonth($guest, $myDate['day']);
         $calls = $this->counterMonth($call, $myDate['day']);
 
-        return @response(['vouchers' => $vouchers, 'guests' => $guests, 'calls' => $calls, 'days' => $myDate['day']]);
+        return response(['vouchers' => $vouchers, 'guests' => $guests, 'calls' => $calls, 'days' => $myDate['day']]);
     }
 
     public function getStatsBySpotPerMonth($id, Request $request)
@@ -67,32 +52,20 @@ class StatMonthRepository implements StatMonthRepositoryInterface
         $new = new Helper();
         $myDate = $new->currentDate($request);
         $spot = Spot::findOrFail($id);
-        $sms = StatsSms::select('date', 'all as all_sms', 'resend', 'delivered')->whereSpot_id($spot->id)
-            ->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
-        $call = StatsCall::select('date', 'requests', 'checked')->whereSpot_id($spot->id)
-            ->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
-        $voucher = StatsVoucher::select('date', 'all', 'auth as auth_vouchers')->whereSpot_id($spot->id)
-            ->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
-        $guest = StatsGuest::select('date', 'load', 'auth as auth_guests', 'new', 'old')->whereSpot_id($spot->id)
-            ->whereMonth('date', $myDate['month'])
-            ->whereYear('date', $myDate['year'])
-            ->get()->toArray();
+        $guest = $this->getGuest($myDate)->whereSpot_id($spot->id)->get()->toArray();
         $guests = $this->counterMonth($guest, $myDate['day']);
         $stats = [];
         switch ($spot->type) {
             case 1:
+                $sms = $this->getSms($myDate)->whereSpot_id($spot->id)->get()->toArray();
                 $stats = $this->counterMonth($sms, $myDate['day']);
                 break;
             case 2:
+                $call = $this->getCall($myDate)->whereSpot_id($spot->id)->get()->toArray();
                 $stats = $this->counterMonth($call, $myDate['day']);
                 break;
             case 3:
+                $voucher = $this->getCall($myDate)->whereSpot_id($spot->id)->get()->toArray();
                 $stats = $this->counterMonth($voucher, $myDate['day']);
                 break;
         }
@@ -100,8 +73,36 @@ class StatMonthRepository implements StatMonthRepositoryInterface
         return response(['guests' => $guests, 'stats' => $stats, 'type' => $spot->type, 'days' => $myDate['day']]);
     }
 
+    public function getSms($myDate)
+    {
+        return StatsSms::select('date', 'all as all_sms', 'resend', 'delivered')
+            ->whereMonth('date', $myDate['month'])
+            ->whereYear('date', $myDate['year']);
+    }
+
+    public function getVoucher($myDate)
+    {
+        return StatsVoucher::select('date', 'all as all_vouchers', 'auth')
+            ->whereMonth('date', $myDate['month'])
+            ->whereYear('date', $myDate['year']);
+    }
+
+    public function getCall($myDate)
+    {
+        return StatsCall::select('date', 'requests', 'checked')
+            ->whereMonth('date', $myDate['month'])
+            ->whereYear('date', $myDate['year']);
+    }
+
+    public function getGuest($myDate)
+    {
+        return StatsGuest::select('date', 'load', 'auth as auth_guests', 'new', 'old')
+            ->whereMonth('date', $myDate['month'])
+            ->whereYear('date', $myDate['year']);
+    }
+
     //@param array $array - массив со статистикой для обработки
-    //@param @number - колличество дней
+    //@param $number - колличество дней
     //@return фильтрованный массив со всей статистикой по дням
 
     public function counterMonth($array, $number)
