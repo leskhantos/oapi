571deb70c5c058603f52c668b43aa6a4155dda45
roman <r.zubik@oyster-telecom.ru>
2020-03-06 17:14:14 +0300

    :bento: Adding paginate for Guests

--- app/Repositories/GuestRepository.php
+++ app/Repositories/GuestRepository.php
@@ -2,10 +2,12 @@
 
 namespace App\Repositories;
 
+use App\Entities\Company;
 use App\Entities\Guests\Guest;
 use App\Helpers\Helper;
 use App\Repositories\Interfaces\GuestRepositoryInterface;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class GuestRepository implements GuestRepositoryInterface
 {
@@ -13,45 +15,54 @@ class GuestRepository implements GuestRepositoryInterface
     {
         $new = new Helper();
         $myDate = $new->currentDate($request);
-        $guests = Guest::select('guests.id','datetime', 'devices.type as type_device', 'os', 'device_mac', 'spots.type', 'data_auth', 'sessions')
+        $company = Company::findOrFail($id);
+        $guests = Guest::select('guests.id', 'datetime', 'devices.type as type_device', 'os', 'device_mac', 'spots.type', 'data_auth', 'sessions')
             ->leftJoin('devices', 'guests.device_mac', '=', 'devices.mac')
             ->leftJoin('spots', 'guests.spot_id', '=', 'spots.id')
-            ->where('guests.spot_id', '=', $id)
+            ->where('guests.spot_id', '=', $company->id)
             ->whereMonth('datetime', $myDate['month'])
             ->whereYear('datetime', $myDate['year'])->orderBy('sessions', 'DESC')
             ->get()->toArray();
 
-        $uniq_mac=[];$uniq_arr=[];
-        foreach ($guests as $guest)
-        {
-            if(!in_array($guest['device_mac'],$uniq_mac)){
-                $uniq_mac[]=$guest['device_mac'];
-                $uniq_arr[]=$guest;
-            }
-        }
-
-        return response($uniq_arr);
+        return $this->getUniqMac($guests);
     }
 
     public function guestByCompany($id, Request $request)
     {
         $new = new Helper();
         $myDate = $new->currentDate($request);
-        $guests = Guest::select('guests.id','datetime', 'devices.type as type_device', 'os', 'device_mac', 'spots.type', 'data_auth', 'sessions')
+        $company = Company::findOrFail($id);
+        $guests = Guest::select('guests.id', 'datetime', 'devices.type as type_device', 'os', 'device_mac', 'spots.type', 'data_auth', 'sessions')
             ->leftJoin('devices', 'guests.device_mac', '=', 'devices.mac')
             ->leftJoin('spots', 'guests.spot_id', '=', 'spots.id')
-            ->where('guests.company_id', '=', $id)
+            ->where('guests.company_id', '=', $company->id)
             ->whereMonth('datetime', $myDate['month'])
-            ->whereYear('datetime', $myDate['year'])->get();
+            ->whereYear('datetime', $myDate['year'])->orderBy('sessions', 'DESC')
+            ->get()->toArray();
 
-        $uniq_mac=[];$uniq_arr=[];
-        foreach ($guests as $guest)
-        {
-            if(!in_array($guest['device_mac'],$uniq_mac)){
-                $uniq_mac[]=$guest['device_mac'];
-                $uniq_arr[]=$guest;
+        return $this->getUniqMac($guests);
+    }
+
+    public function getUniqMac($guests)
+    {
+        $uniq_mac = [];
+        $uniq_arr = [];
+        foreach ($guests as $guest) {
+            if (!in_array($guest['device_mac'], $uniq_mac)) {
+                $uniq_mac[] = $guest['device_mac'];
+                $uniq_arr[] = $guest;
             }
         }
-        return response($uniq_arr);
+
+        $total = count($uniq_arr);
+        $perPage = 5;
+        $currentPage = 1;
+
+        $paginator = new LengthAwarePaginator($uniq_arr, $total, $perPage, $currentPage);
+        $paginator = $paginator->toArray();
+
+        $data = $paginator['data'];
+        $meta = ['current_page' => $paginator['current_page'], 'total' => $paginator['total'], 'per_page' => $paginator['per_page']];
+        return response(['data' => $data, 'meta' => $meta]);
     }
 }
