7804c9d7bf0720e79054d9eafbc9bf5cedfb552e
roman <r.zubik@oyster-telecom.ru>
2020-03-12 18:50:34 +0300

    :ambulance: Critical.

--- app/Http/Controllers/SpotController.php
+++ app/Http/Controllers/SpotController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Entities\Device;
 use App\Entities\GuestCall;
+use App\Entities\SessionsAuth;
 use App\Entities\Sms;
 use App\Entities\Spot;
+use App\Entities\Stage;
+use App\Entities\UserAgent;
+use App\Helpers\Helper;
 use App\Http\Requests\Api\Spot\SpotsStoreRequest;
 use App\Http\Requests\Api\Spot\SpotsUpdateRequest;
 use App\Repositories\Interfaces\SpotRepositoryInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SpotController extends Controller
 {
@@ -65,25 +71,93 @@ class SpotController extends Controller
         return Spot::create($request->all());
     }
 
-    public function saveLogsBySpot(Request $request)
+    public function saveLogsBySpot(Request $request) // Тупо запись в файл данных
     {
-//        $id = $request->id;
-//        $spot = Spot::findOrFail($id);
-//        $name = strtolower($spot->ident);
-//        $name .= ".log";
-//        $way="/home/roman/valet/api.oyspot/www/data/spots/$name";
-//        $array = $request->request;
-//
-//        $result="";
-//        foreach ($array as $arr){
-//            $result .= "$arr|";
-//        }
-//        $data = "$result\r\n";
-//
-//        \File::append("$way", "$data");
+        $date = new \DateTime();
+        $ident = $request->v1;
+        $name = $request->v2;
+        $mac = $request->v3;
+        $ip = $request->v4;
+        $spot = Spot::whereIdent($ident)->first();
+        if (!$spot) {
+            return response('F', 404);
+        }
+
+        $device = Device::whereMac($mac)->first();
+
+        $User['info'] = filter_input(INPUT_SERVER, 'HTTP_USER_AGENT', FILTER_SANITIZE_SPECIAL_CHARS);
+        $DevInfoHash = md5($User['info']);
+
+        $User['signature'] = md5($name . $mac . $ip . 'TooManySecrets');
+
+        $array = $request->all();
+
+//        Есть в devices по mac?
+//        Нет - записываем с дефолтными полями
+
+        if ($device) {
+            Device::whereMac($mac)->update(['created' => $date]);
+        } else {
+            Device::create(['created' => $date, 'mac' => $mac]);
+        }
+
+//        Проверяем наличие записи DeviceInfo в таблице user - agents
+//        Есть - обновляем запись в devices
+//        Нет - добавляем с дефолтными значениями
+//    Есть - обновляем запись
+
+        $userinfo = UserAgent::whereUid($DevInfoHash)->first();
+        if ($userinfo) {
+            UserAgent::whereUid($DevInfoHash)->update(['info' => $User['info']]);
+        } else {
+            UserAgent::create(['uid' => $DevInfoHash, 'info' => $User['info']]);
+        }
+
+
+//  Проверяем наличие записи в sessions . auth(соответствие зоны, мака и сигнатуры, не истекший expiration)
+        //    Есть - обновляем counter и авторизируем устройство
+
+        $session = SessionsAuth::whereSpot_id($spot->id)->whereDevice_mac($mac)
+            ->whereSignature($User['signature'])->where('expiration', '>', $date)->first();
+        if ($session) {
+            $count = $session->counter;
+            $count = $count + 1;
+            SessionsAuth::whereMac($mac)->update(['created' => $date, 'counter' => $count]);
+        }
+
+//  Проверяем наличие записи в stages
+//    Есть - передаем фронту данные записи
+
+        $stages = Stage::whereSpot_id($spot->id)->whereDevice_mac($mac)->first();
+        if ($stages) {
+            return 123;
+        }
+
+        $vibor = [1, 2, 3];
+        $phone ="";  // ДОЛЖЕН ПРИЙТИ
+        switch ($vibor) {
+            case 1:
+                break;//SMS
+            case 2:
+                GuestCall::create(['phone' => 123]);
+                Stage::create(['created' => $date, 'spot_id' => $spot->id, 'device_mac' => $mac, 'phone' =>$phone]);
+                break;//CALL
+            case 3:
+                break;//VOUCHER
+        }
+
+        $name = strtolower($spot->ident);
+        $name .= ".log";
+
+        $result = "";
+        foreach ($array as $arr) {
+            $result .= "$arr|";
+        }
+
+        Storage::append($name, $result);
+
 //        $contents =\File::get("$way");
-//
-//        return $contents;
+
         return view('test', $request->all());
     }
 
