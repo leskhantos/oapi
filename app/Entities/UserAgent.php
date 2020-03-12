09eed81df95f69a875dc93ebe74388944a265991
roman <r.zubik@oyster-telecom.ru>
2020-03-12 18:51:09 +0300

    fxs

--- app/Entities/UserAgent.php
+++ app/Entities/UserAgent.php
@@ -9,7 +9,7 @@ class UserAgent extends Model
     public $timestamps = false;
 
     protected $fillable = [
-        'info', 'software', 'software_name', 'software_code',
+        'uid','info', 'software', 'software_name', 'software_code',
         'software_version', 'software_type', 'engine_name',
         'engine_version', 'hardware_type', 'os',
         'os_name', 'os_code', 'os_version'
