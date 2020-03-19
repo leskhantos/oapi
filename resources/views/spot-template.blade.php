5d8a5e82943ce1edae5295040e948e4241531e17
Лесхан <l.karataev@oyster-telecom.ru>
2020-03-19 19:57:04 +0300

    :heavy_plus_sign: Adding loader.

--- resources/views/spot-template.blade.php
+++ resources/views/spot-template.blade.php
@@ -10,14 +10,7 @@
 <body>
   <div id="spot-template">
     <spot-template
-        :v1="{{$v1}}"
-        :v2="{{$v2}}"
-        :v3="{{$v3}}"
-        :v4="{{$v4}}"
-        :v5="{{$v5}}"
-        :v6="{{$v6}}"
-        :v7="{{$v7}}"
-        :v8="{{$v8}}"
+        :data="{{ json_encode($data) }}"
     />
   </div>
   <script src="{{ mix('js/app.js') }}"></script>
--- routes/api.php
+++ routes/api.php
@@ -90,13 +90,15 @@ Route::post('/test/{id}', 'TestController@index');
 
 Route::get('enter/test', function () {
     return view('spot-template', [
-        'v1' => '1',
-        'v2' => '2',
-        'v3' => '3',
-        'v4' => '4',
-        'v5' => '5',
-        'v6' => '6',
-        'v7' => '7',
-        'v8' => '8',
+        'data' => [
+            'v1' => '1',
+            'v2' => '2',
+            'v3' => '3',
+            'v4' => '4',
+            'v5' => '5',
+            'v6' => '6',
+            'v7' => '7',
+            'v8' => '8',
+        ]
     ]);
 });
