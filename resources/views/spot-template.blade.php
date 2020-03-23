f6e790eb3d1849f7ca53e344feedf52156a36d43
Лесхан <l.karataev@oyster-telecom.ru>
2020-03-23 11:41:50 +0300

    :bento: Adding favicon and title.

--- resources/views/spot-template.blade.php
+++ resources/views/spot-template.blade.php
@@ -4,8 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-  <title>Document</title>
+    <link rel="icon" href="{{ URL::asset('favicon-16x16.png') }}" type="image/png" sizes="16x16"/>
+    <link rel="icon" href="{{ URL::asset('favicon-32x32.png') }}" type="image/png" sizes="32x32"/>
+    <link rel="icon" href="{{ URL::asset('favicon-96x96.png') }}" type="image/png" sizes="96x96"/>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <title>HotSpot</title>
 </head>
 <body>
   <div id="spot-template">
