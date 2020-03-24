d37106530c3797e0e007c54647f655a39e35367d
Valitov Timur <t.valitov@oy2b.ru>
2020-03-24 11:34:01 +0300

    Update

--- resources/views/spot-template.blade.php
+++ resources/views/spot-template.blade.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{ URL::asset('favicon-16x16.png') }}" type="image/png" sizes="16x16"/>
-    <link rel="icon" href="{{ URL::asset('favicon-32x32.png') }}" type="image/png" sizes="32x32"/>
-    <link rel="icon" href="{{ URL::asset('favicon-96x96.png') }}" type="image/png" sizes="96x96"/>
+    <link rel="icon" href="{{ env('APP_URL') . env('APP_RESOURCES_DIR') . 'favicon-16x16.png' }}" type="image/png" sizes="16x16"/>
+    <link rel="icon" href="{{ env('APP_URL') . env('APP_RESOURCES_DIR') . 'favicon-32x32.png' }}" type="image/png" sizes="32x32"/>
+    <link rel="icon" href="{{ env('APP_URL') . env('APP_RESOURCES_DIR') . 'favicon-96x96.png' }}" type="image/png" sizes="96x96"/>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   <title>HotSpot</title>
 </head>
