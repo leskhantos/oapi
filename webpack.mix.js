065f7644b8c68014f14ae98388de80753f53de6b
Valitov Timur <t.valitov@oy2b.ru>
2020-03-24 11:32:11 +0300

    Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 const mix = require("laravel-mix");
 require("dotenv").config();
 
+console.log(process.env.MIX_RESOURCES_ROOT);
+
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/scss/app.scss", "public/css")
-    .setResourceRoot(process.env.MIX_RESOURCES_ROOT);
+    .setResourceRoot(process.env.APP_RESOURCES_DIR);
