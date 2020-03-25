edd8347808253348d9023ea048a77a8815e5a29c
roman <r.zubik@oyster-telecom.ru>
2020-03-25 13:47:26 +0300

    :building_construction: Making architectural changes.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->enum('type', ['admin', 'manager', 'support']);
-            $table->string('name', 150);
+            $table->enum('type', ['admin', 'manager', 'support', 'client']);
+            $table->integer('id_company')->default(0);
+            $table->string('name', 150)->nullable();
             $table->string('login', 20)->unique()->index();
-            $table->string('password',60);
+            $table->string('password', 60);
             $table->dateTime('last_online')->nullable();
             $table->string('last_ip', 50)->nullable();
             $table->boolean('enabled')->default(true);
