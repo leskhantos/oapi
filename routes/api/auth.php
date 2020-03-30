a6397e8e52a194cc7b75d34fb843194a0ae2bab6
roman <r.zubik@oyster-telecom.ru>
2020-03-30 18:05:10 +0300

    logout fixed

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::post('login', 'AuthController@login');
-Route::match(['get', 'post'], 'logout', 'AuthController@logout');
+Route::match(['get', 'post'], 'logout', 'AuthController@logout')->middleware('auth:api');
 Route::get('user', 'AuthController@user')->middleware('auth:api');
